367d0825b8f996d7804d328b7c071e9c918c5125
Alex Hartel <ahartel@cs.unm.edu>
2024-09-20 22:44:16 -0600

    LEDBANK

--- src/LEDBank.java
+++ src/LEDBank.java
@@ -1,2 +1,50 @@
 public class LEDBank {
+    private boolean heatingLED;
+    private boolean reservoirLED;
+    private boolean voltageLED;
+    private boolean errorLED;
+    private boolean brewLED;
+
+    public LEDBank(){
+        heatingLED = false;
+        reservoirLED = false;
+        voltageLED = false;
+        errorLED = false;
+        brewLED = false;
+    }
+    // Heating
+    public void setHeatingLED(boolean input){
+        heatingLED = input;
+    }
+    public boolean getHeatingLED(){
+        return heatingLED;
+    }
+    // Reservoir
+    public void setReservoirLED(boolean input){
+        reservoirLED = input;
+    }
+    public boolean getReservoirLED(){
+        return reservoirLED;
+    }
+    // Voltage
+    public void setVoltageLED(boolean input){
+        voltageLED = input;
+    }
+    public boolean getVoltageLED(){
+        return voltageLED;
+    }
+    // Error
+    public void setErrorLED(boolean input){
+        errorLED = input;
+    }
+    public boolean getErrorLED(){
+        return errorLED;
+    }
+    // BREW
+    public void setBrewLED(boolean input){
+        brewLED = input;
+    }
+    public boolean getBrewLED(){
+        return brewLED;
+    }
 }
