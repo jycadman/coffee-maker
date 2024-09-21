f5d5b73fe6d1b93dd42d4fb2602dcfb1244c4bca
Alex Hartel <ahartel@cs.unm.edu>
2024-09-20 22:14:14 -0600

    Heater

--- src/Heater.java
+++ src/Heater.java
@@ -1,2 +1,25 @@
 public class Heater {
+    private int heatTemperature;
+    private boolean heaterStatus;
+    public Heater(){
+        heaterStatus = false;
+        heatTemperature = 72;
+    }
+
+    public void setHeaterTemperature(int input){
+        heatTemperature = input;
+    }
+
+    public void incrementByOne(){
+        heatTemperature+=1;
+    }
+
+    public void decrementByOne(){
+        heatTemperature-=1;
+    }
+
+    public int getHeatTemperature(){
+        return heatTemperature;
+    }
+
 }
