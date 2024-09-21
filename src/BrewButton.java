54e6af53dd8c9b37244d39bc46f1c09d4f281cd1
Alex Hartel <ahartel@cs.unm.edu>
2024-09-20 21:41:37 -0600

    BrewButton

--- src/BrewButton.java
+++ src/BrewButton.java
@@ -1,4 +1,18 @@
 // Temp Sensor, Reservoir Sensor, Carafe Sensor, LED Bank
 public class BrewButton {
-    
+    private TemperatureSensor temperatureSensor;
+    private ReservoirSensor reservoirSensor;
+    private CarafeSensor carafeSensor;
+    private Timer timer;
+
+    public BrewButton(){
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+        ReservoirSensor reservoirSensor = new ReservoirSensor();
+        CarafeSensor carafeSensor = new CarafeSensor();
+        Timer timer = new Timer();
+    }
+
+
+
+
 }
