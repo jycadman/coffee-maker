4e47cc436f85bbd5c21c1f95148d250c289c8a32
william <lopezw@unm.edu>
2024-09-20 20:53:20 -0600

    Added Classes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
+    public static  void standBy() {
+
+    }
+
+    public static void brewing(){
+
+    }
+
+    public static void heating(){
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
