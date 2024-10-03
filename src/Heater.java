e25770de3ed1c68164b646bd6efddbc2f2b3d53f
william <lopezw@unm.edu>
2024-10-03 09:43:06 -0600

    Changed devices to match SAD

--- src/Heater.java
+++ src/Heater.java
@@ -1,9 +1,11 @@
+import java.io.PrintWriter;
+
 public class Heater {
-    public String heatUp() {
+    public String heatUp(PrintWriter writer) {
         return "HHU";
     }
 
-    public String coolDown() {
+    public String coolDown(PrintWriter writer) {
         return "HCD";
     }
 }
